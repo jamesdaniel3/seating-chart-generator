307e5ecaa3c78dd948d54ec669fff734f7add507
jamesdaniel3 <jamesmd333@gmail.com>
2024-01-23 23:59:51 -0500

    replaced comments with triple quoted strings, more readable

--- assign_partners.py
+++ assign_partners.py
@@ -5,15 +5,26 @@ import seating_display
 import roster_parsing
 
 
-# sect_dict : {section number : room, ...}
+"""
+SECT_DICT is a dictionary where each key is a section number and each value is the room that the section's lab is held in.
+"""
+
 SECT_DICT = {100: "ols018", 101: "ols018", 102: "ols018", 103: "ols018", 104: "ols009", 105: "ols018"}
 
-# room_info : {room : (columns of tables, number of tables, (table width, table height), max student per table), ...},
+
+"""
+STANDARD_ROOMS_INFO is a dictionary of rooms where each value is a tuple of the following format: 
+(columns of tables, number of tables, (table width, table height), max student per table)
+"""
+
 STANDARD_ROOMS_INFO = {"ols018": (5, 15, (200, 200), 6),
               "ols005": (2, 8, (400, 200), 6),
               "mech215": (4, 4, (200, 500), 6)}
 
-# irregular_rooms_info : {room : (columns or tables, array of TOTAL students per row), ... },
+"""
+IRREGULAR_ROOMS_INFO is a dictionary of rooms where each value is a nested list. Each sublist is a row of tables 
+and each value in the sublist is the number of students the row can hold 
+"""
 IRREGULAR_ROOMS_INFO = {"ols009": [[6, 6], [7,7], [7,8], [8,9], [6,4], [4] ] }
 
 REVERSE = False
@@ -40,6 +51,3 @@ else:
 
 win.mainloop()
 
-
-# to document:
-    # functionality of reverse
