e4f23f52e1d8c0e4dd64d449305c43b87bccbd8d
jamesdaniel3 <jamesmd333@gmail.com>
2024-01-24 01:05:15 -0500

    removed irrelevant dictionaries and improved comments

--- assign_partners.py
+++ assign_partners.py
@@ -13,19 +13,10 @@ SECT_DICT = {100: "ols018", 101: "ols018", 102: "ols018", 103: "ols018", 104: "o
 
 
 """
-STANDARD_ROOMS_INFO is a dictionary of rooms where each value is a tuple of the following format: 
-(columns of tables, number of tables, (table width, table height), max student per table)
+ROOM_INFO is a dictionary of rooms where each value is a tuple containing a nested list and a boolean value. 
+Each sublist of the nested list represents a row of tables and each value in the sublist is the number of students the 
+row can hold. The boolean value indicates whether a room is standard or irregular.  
 """
-
-# STANDARD_ROOMS_INFO = {"ols018": [[6,6,6,6,6], [6,6,6,6,6], [6,6,6,6,6]],
-#               "ols005": (2, 8, (400, 200), 6),
-#               "mech215": (4, 4, (200, 500), 6)}
-
-"""
-IRREGULAR_ROOMS_INFO is a dictionary of rooms where each value is a nested list. Each sublist is a row of tables 
-and each value in the sublist is the number of students the row can hold 
-"""
-#IRREGULAR_ROOMS_INFO = {"ols009": [[6, 6], [7,7], [7,8], [8,9], [6,4], [4]]}
 
 ROOM_INFO = {
     "ols009": ([
@@ -74,13 +65,7 @@ win = tkinter.Tk()
 win.title('Lab ' + argv[1])
 
 tables = seating_selection.seating_selection(current_room_info, students)
-seating_display.irregular_seating_display(current_room_info, tables, win, REVERSE)
-
-# if not ROOM[1]:
-#     seating_display.irregular_seating_display(current_room_info, tables, win, REVERSE)
-#
-# else:
-#     seating_display.irregular_seating_display(current_room_info, tables, win, REVERSE)
+seating_display.seating_display(current_room_info, tables, win, REVERSE)
 
 win.mainloop()
 
