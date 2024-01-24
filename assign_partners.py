fd0ee6ee1234db7a9d35faf1d6ba102626c3db52
jamesdaniel3 <jamesmd333@gmail.com>
2024-01-24 01:19:02 -0500

    imported new file and stored the dictionaries in variables

--- assign_partners.py
+++ assign_partners.py
@@ -3,59 +3,18 @@ import tkinter
 import seating_selection
 import seating_display
 import roster_parsing
+import section_and_room_info
 
+SECT_DICT = section_and_room_info.SECT_DICT
+ROOM_DICT = section_and_room_info.ROOM_DICT
 
-"""
-SECT_DICT is a dictionary where each key is a section number and each value is the room that the section's lab is held in.
-"""
-
-SECT_DICT = {100: "ols018", 101: "ols018", 102: "ols018", 103: "ols018", 104: "ols009", 105: "ols018"}
-
-
-"""
-ROOM_INFO is a dictionary of rooms where each value is a tuple containing a nested list and a boolean value. 
-Each sublist of the nested list represents a row of tables and each value in the sublist is the number of students the 
-row can hold. The boolean value indicates whether a room is standard or irregular.  
-"""
-
-ROOM_INFO = {
-    "ols009": ([
-                   [6, 6],
-                   [7,7],
-                   [7,8],
-                   [8,9],
-                   [6,4],
-                   [4]
-               ], False),
-    "ols018": ([
-                   [6,6,6,6,6],
-                   [6,6,6,6,6],
-                   [6,6,6,6,6]
-               ], True),
-    "ols005": ([
-                   [6,6],
-                   [6,6],
-                   [6,6],
-                   [6,6],
-                   [6,6],
-                   [6,6],
-                   [6,6],
-                   [6,6],
-               ], True),
-    "mech215": ([
-                    [6,6],
-                    [6,6],
-                    [6,6],
-                    [6,6],
-                ], True)
-}
 
 REVERSE = False
 
 if len(argv) >= 3 and (argv[2] == "-r" or argv[2] == "--reverse"):
     REVERSE = True
 
-data = roster_parsing.read_csv(SECT_DICT, ROOM_INFO)
+data = roster_parsing.read_csv(SECT_DICT, ROOM_DICT)
 
 ROOM = data[0]
 current_room_info = data[1]
