207890657c556d4c0e55e5d128ac15fc4bd44ac3
jamesdaniel3 <jamesmd333@gmail.com>
2024-01-24 00:33:26 -0500

    added remaining rooms to new ROOM_INFO dict and renamed a couple variables

--- assign_partners.py
+++ assign_partners.py
@@ -27,30 +27,58 @@ and each value in the sublist is the number of students the row can hold
 """
 #IRREGULAR_ROOMS_INFO = {"ols009": [[6, 6], [7,7], [7,8], [8,9], [6,4], [4]]}
 
-TESTER_ROOM_INFO = {"ols009": ([[6, 6], [7,7], [7,8], [8,9], [6,4], [4]], False),
-                    "ols018": ([[6,6,6,6,6], [6,6,6,6,6], [6,6,6,6,6]], True)}
+ROOM_INFO = {"ols009": ([
+                                   [6, 6],
+                                   [7,7],
+                                   [7,8],
+                                   [8,9],
+                                   [6,4],
+                                   [4]
+                               ], False),
+                    "ols018": ([
+                                   [6,6,6,6,6],
+                                   [6,6,6,6,6],
+                                   [6,6,6,6,6]
+                               ], True),
+                    "ols005": ([
+                                   [6,6],
+                                   [6,6],
+                                   [6,6],
+                                   [6,6],
+                                   [6,6],
+                                   [6,6],
+                                   [6,6],
+                                   [6,6],
+                               ], True),
+                    "mech215": ([
+                                    [6,6],
+                                    [6,6],
+                                    [6,6],
+                                    [6,6],
+                                ], True)
+                    }
 
 REVERSE = False
 
 if len(argv) >= 3 and (argv[2] == "-r" or argv[2] == "--reverse"):
     REVERSE = True
 
-data = roster_parsing.read_csv(SECT_DICT, TESTER_ROOM_INFO)
+data = roster_parsing.read_csv(SECT_DICT, ROOM_INFO)
 
 ROOM = data[0]
-ROOM_INFO = data[1]
+current_room_info = data[1]
 students = data[2]
 
 win = tkinter.Tk()
 win.title('Lab ' + argv[1])
 
-tables = seating_selection.seating_selection(ROOM_INFO, students)
+tables = seating_selection.seating_selection(current_room_info, students)
 
 if not ROOM[1]:
-    seating_display.irregular_seating_display(ROOM_INFO, tables, win, REVERSE)
+    seating_display.irregular_seating_display(current_room_info, tables, win, REVERSE)
 
 else:
-    seating_display.irregular_seating_display(ROOM_INFO, tables, win, REVERSE)
+    seating_display.irregular_seating_display(current_room_info, tables, win, REVERSE)
 
 win.mainloop()
 
