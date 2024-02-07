382578f4b9e9fba2187e882ec18e3f0d70ca96ef
jamesdaniel3 <jamesmd333@gmail.com>
2024-02-06 19:37:54 -0500

    placed the check logic in a function

--- check_necessary_packages.py
+++ check_necessary_packages.py
@@ -4,10 +4,11 @@ def install_package(package):
     subprocess.check_call(["python", "-m", "pip", "install", package])
 
 
-required_package = "pyautogui"
-try:
-    import pyautogui
-except ImportError:
-    print(f"Required package '{required_package}' is not installed. Installing it now...")
-    install_package(required_package)
-    import pyautogui  # Now import again after installation
+def check_required_packages():
+    required_package = "pyautogui"
+    try:
+        import pyautogui
+    except ImportError:
+        print(f"Required package '{required_package}' is not installed. Installing it now...")
+        install_package(required_package)
+        import pyautogui  # Now import again after installation
