6f9a9d75182bd4ea35aa802dd89ae6c0d7050361
jamesdaniel3 <jamesmd333@gmail.com>
2024-02-06 19:32:04 -0500

    created file that will install the package necessary for screenshotting on windows if it is not already in place on the users computer

---
+++ check_necessary_packages.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def install_package(package):
+    subprocess.check_call(["python", "-m", "pip", "install", package])
+
+
+required_package = "pyautogui"
+try:
+    import pyautogui
+except ImportError:
+    print(f"Required package '{required_package}' is not installed. Installing it now...")
+    install_package(required_package)
+    import pyautogui  # Now import again after installation
